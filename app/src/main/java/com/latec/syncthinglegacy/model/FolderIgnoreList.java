3d28d06f6b2b654fbf332007b50aff4d39c583df
Andrei Ladyka <andrei@ladyka.by>
2025-01-28 00:15:47 +0300

    Change package name to com.latec.syncthinglegacy

---
+++ app/src/main/java/com/latec/syncthinglegacy/model/FolderIgnoreList.java
@@ -0,0 +1,10 @@
+package com.latec.syncthinglegacy.model;
+
+/**
+ * To avoid name confusion:
+ * This is the exclude and include items list associated with every folder.
+ */
+public class FolderIgnoreList {
+    public String[] expanded;
+    public String[] ignore;
+}
---
+++ app/src/main/java/com/latec/syncthinglegacy/model/SystemVersion.java
@@ -0,0 +1,9 @@
+package com.latec.syncthinglegacy.model;
+
+public class SystemVersion {
+    public String arch;
+    public String codename;
+    public String longVersion;
+    public String os;
+    public String version;
+}
